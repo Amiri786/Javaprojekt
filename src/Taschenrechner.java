ce1fd5449bc3d8c0da7816fcc70f419a0247f81f
Abdullah Amiri <Abdullah.Amiri@gmx.de>
2022-06-22 00:43:51 +0200

    if statment für plus brechnen

--- src/Taschenrechner.java
+++ src/Taschenrechner.java
@@ -9,14 +9,15 @@ public class Taschenrechner implements ActionListener {
         JFrame frame;
         JTextField textField;
         JButton[] numberButton = new JButton[10];
-        JButton[] functionButtons= new JButton[8];
+        JButton[] functionButtons= new JButton[9];
         JButton addButton,subButton,mulButton,divButton;
-        JButton decButton, equButton, delButton, clrButton;
+        JButton decButton, equButton, delButton, clrButton,negButton;
         JPanel panel;
 
         Font myFont = new Font ("Ink Free", Font.BOLD,30);
         double num1 = 0, num2 = 0, num3 = 0;
         char operator;
+        private double result;
 
         Taschenrechner(){
         frame = new JFrame("Taschenrechner");
@@ -38,6 +39,8 @@ public class Taschenrechner implements ActionListener {
                 equButton = new JButton("=");
                 delButton = new JButton("Delete");
                 clrButton = new JButton("Clear");
+                clrButton = new JButton("Clear");
+                negButton= new JButton("(-)");
 
                 functionButtons[0] = addButton;
                 functionButtons[1] = subButton;
@@ -47,8 +50,9 @@ public class Taschenrechner implements ActionListener {
                 functionButtons[5] = equButton;
                 functionButtons[6] = delButton;
                 functionButtons[7] = clrButton;
+                functionButtons[8] = negButton;
 
-                for(int i =0;i<8;i++){
+                for(int i =0;i<9;i++){
                         functionButtons[i].addActionListener(this);
                         functionButtons[i].setFont(myFont);
                         functionButtons[i].setFocusable(false);
@@ -61,8 +65,9 @@ public class Taschenrechner implements ActionListener {
                         numberButton[i].setFocusable(false);
 
                 }
-                delButton.setBounds(50,430,145,50);
-                clrButton.setBounds(205,430,145,50);
+                negButton.setBounds(50,430,100,50);
+                delButton.setBounds(150,430,100,50);
+                clrButton.setBounds(250,430,100,50);
 
                 panel= new JPanel();
                 panel.setBounds(50,100,300,300);
@@ -89,7 +94,7 @@ public class Taschenrechner implements ActionListener {
                 panel.add(divButton);
 
                 frame.add(panel);
-
+                frame.add(negButton);
                 frame.add(delButton);
                 frame.add(clrButton);
                 frame.add(textField);
@@ -115,5 +120,61 @@ public class Taschenrechner implements ActionListener {
         if(e.getSource()==decButton){
                 textField.setText(textField.getText().concat("."));
         }
+                if(e.getSource()==addButton){
+                        num1= Double.parseDouble(textField.getText());
+                        operator = '+';
+                        textField.setText("");
+                }
+                if(e.getSource()==subButton){
+                        num1= Double.parseDouble(textField.getText());
+                        operator = '-';
+                        textField.setText("");
+                }
+                if(e.getSource()==mulButton){
+                        num1= Double.parseDouble(textField.getText());
+                        operator = '*';
+                        textField.setText("");
+                }
+                if(e.getSource()==divButton){
+                        num1= Double.parseDouble(textField.getText());
+                        operator = '/';
+                        textField.setText("");
+                }
+                if(e.getSource()==equButton){
+                        num2=Double.parseDouble(textField.getText());
+                       switch (operator){
+                               case'+':
+                                      result = num1 + num2;
+                                       break;
+                               case'-':
+                                    result = num1 - num2;
+                                       break;
+                               case'*':
+                                       result = num1 * num2;
+                                       break;
+                               case'/':
+                                       result = num1 / num2;
+                                       break;
+                       }
+                       textField.setText(String.valueOf(result));
+                       num1=result;
+                }
+                if(e.getSource()==clrButton){
+                     textField.setText("");
+                }
+                if(e.getSource()==delButton){
+                       String string = textField.getText();
+                       textField.setText("");
+                       for(int i=0;i<string.length()-1;i++){
+                               textField.setText(textField.getText()+string.charAt(i));
+                       }
+                }
+                if(e.getSource()==negButton){
+                  double temp= Double.parseDouble(textField.getText());
+                  temp*=-1;
+                  textField.setText(String.valueOf(temp));
+                        }
+
+
         }
 }
